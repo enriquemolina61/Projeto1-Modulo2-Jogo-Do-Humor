a06873598633a983c8f5e63431292b0be953cb02
Enrique Molina <76474854+enriquemolina61@users.noreply.github.com>
2022-04-16 17:34:42 -0300

    commit

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+const imgslist = [
+    "./assets/img/relaxado.gif",
+    "./assets/img/concentrado.gif",
+    "./assets/img/sementender.gif",
+    "./assets/img/neyday.gif",
+    ];
+const situacao = document.querySelector("#situacao");
+const img = document.querySelector("img");
+const button = document.querySelector(".button");
+const nome = document.querySelector("#nome");
+
+// button.addEventListener("click", function() {
+
+// });
+let mudanca = 0;
+function alterar() {
+  if (mudanca == 0) {
+    nome.innerText = "Neymar Jr PSG";
+    situacao.innerText = "Concentrado";
+    img.src = imgslist[1]
+  } else if (mudanca == 1) {
+    nome.innerText = "Neymar Jr";
+    situacao.innerText = "Supreso";
+    img.src = imgslist[2]
+  } else if (mudanca == 2) {
+    nome.innerText = "Ney Day";
+    situacao.innerText = "OnFire";
+    img.src = imgslist[3]
+  } else if (mudanca == 3) {
+    nome.innerText = "Neymar Jr Barcelona";
+    situacao.innerText = "Relaxado ";
+    img.src = imgslist[0]
+
+  }
+  mudanca++;
+  if (mudanca >= 4) {
+    mudanca = 0;
+  }
+}
